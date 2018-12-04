5177beaf58c2b12c789c0c85d674558c2a255c84
Ross Williams <ross@ross-williams.net>
2018-12-04 10:30:28 -0500

    net-mgmt/zabbix34-libzbxmodbus: Fix some build issues

--- net-mgmt/zabbix34-libzbxmodbus/files/patch-libmodbus_src_modbus-data.c
+++ net-mgmt/zabbix34-libzbxmodbus/files/patch-libmodbus_src_modbus-data.c
@@ -1,9 +1,9 @@
---- libmodbus/src/modbus-data.c 2018-09-24 09:58:31.000000000 +0000
-+++ libmodbus/src/modbus-data.c  2018-12-03 21:04:36.280902000 +0000
+--- libmodbus/src/modbus-data.c.orig	2018-09-24 09:58:31 UTC
++++ libmodbus/src/modbus-data.c
 @@ -36,6 +36,13 @@
  #  define bswap_64 OSSwapInt64
  #endif
-
+ 
 +#if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
 +#  include <sys/endian.h>
 +#  define bswap_16 bswap16
--- net-mgmt/zabbix34-libzbxmodbus/files/patch-src_modbus.c
+++ net-mgmt/zabbix34-libzbxmodbus/files/patch-src_modbus.c
@@ -1,18 +1,18 @@
---- src/modbus.c        2018-09-24 09:58:30.000000000 +0000
-+++ src/modbus.c 2018-12-03 21:13:58.828691000 +0000
-@@ -53,12 +53,9 @@
-
+--- src/modbus.c.orig	2018-09-24 09:58:30 UTC
++++ src/modbus.c
+@@ -53,12 +53,9 @@ int MODBUS_SEM_ID = -1;
+ 
  #define MAX_RETRIES 10
-
+ 
 -union semun
 -{
--       int              val;
--       struct semid_ds *buf;
--       unsigned short * array;
+-	int		 val;
+-	struct semid_ds *buf;
+-	unsigned short * array;
 -};
 +#ifndef ETIME
 +#  define ETIME ETIMEDOUT
 +#endif
-
+ 
  /* the variable keeps timeout setting for item processing */
  static int item_timeout = 0;
