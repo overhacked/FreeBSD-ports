77fab8c8e5cf41bcb1274e1940de7bdd6c840018
Ross Williams <ross@ross-williams.net>
2018-12-14 21:05:56 -0500

    zabbix34-libzbxmodbus: Got src/modbus.c patch wrong

--- net-mgmt/zabbix34-libzbxmodbus/files/patch-src_modbus.c
+++ net-mgmt/zabbix34-libzbxmodbus/files/patch-src_modbus.c
@@ -1,18 +1,13 @@
---- src/modbus.c.orig	2018-09-24 09:58:30 UTC
+--- src/modbus.c.orig	2018-12-15 02:00:43 UTC
 +++ src/modbus.c
-@@ -53,12 +53,9 @@ int MODBUS_SEM_ID = -1;
+@@ -60,6 +60,10 @@ union semun
+ 	unsigned short * array;
+ };
  
- #define MAX_RETRIES 10
- 
--union semun
--{
--	int		 val;
--	struct semid_ds *buf;
--	unsigned short * array;
--};
 +#ifndef ETIME
 +#  define ETIME ETIMEDOUT
 +#endif
- 
++
  /* the variable keeps timeout setting for item processing */
  static int item_timeout = 0;
+ 
